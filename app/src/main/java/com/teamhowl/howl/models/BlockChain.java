54472e5486b148692f6728e5a291b5922ae31cef
Jason <jpanovka@uoguelph.ca>
2022-02-17 11:53:34 -0500

    Created Java functions/stubs

--- app/src/main/java/com/teamhowl/howl/models/BlockChain.java
+++ app/src/main/java/com/teamhowl/howl/models/BlockChain.java
@@ -1,4 +1,6 @@
 package com.teamhowl.howl.models;
 
 public class BlockChain {
+
+    private char chatId;
 }
--- app/src/main/java/com/teamhowl/howl/models/Message.java
+++ app/src/main/java/com/teamhowl/howl/models/Message.java
@@ -1,4 +1,22 @@
 package com.teamhowl.howl.models;
 
+import java.util.Date;
+
 public class Message {
+
+    private String text;
+    private Date timeSent;
+    private Date timeReceived;
+
+    public Message(String text, Date timeSent, Date timeReceived){
+
+        this.text = text;
+        this.timeSent = timeSent;
+        this.timeReceived = timeReceived;
+    }
+
+    public String getText(){ return text; }
+    public Date getTimeSent(){ return timeSent; }
+    public Date getReceived(){ return timeReceived; }
+
 }
