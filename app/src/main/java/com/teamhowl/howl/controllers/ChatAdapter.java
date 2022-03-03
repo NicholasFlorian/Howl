8fc3fcbe7e4fb92cbdeac5546b8ec4682c392703
Jason <jpanovka@uoguelph.ca>
2022-03-03 12:03:34 -0500

    ChatRoom ListViewAdapter
    Created a listview adapter for the ChatRoom Class

---
+++ app/src/main/java/com/teamhowl/howl/controllers/ChatAdapter.java
@@ -0,0 +1,50 @@
+package com.teamhowl.howl.controllers;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.teamhowl.howl.models.ChatRoom;
+import com.teamhowl.howl.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.teamhowl.howl.R;
+
+public class ChatAdapter extends ArrayAdapter<ChatRoom> {
+    public ChatAdapter(Context context, List<ChatRoom> chatRooms) {
+        super(context, 0, chatRooms);
+    }
+
+    @Override
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Get the data item for this position
+        ChatRoom chatRoom = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.cardview_connect_user,
+                    parent,
+                    false);
+        }
+        // Lookup view for data population
+        //TextView userName = (TextView) convertView.findViewById(R.id.connect_user_name);
+        //TextView chatId = (TextView) convertView.findViewById(R.id.connect_chat_id);
+
+        // Populate the data into the template view using the data object
+        //userName.setText(user.getUserName());
+        //chatId.setText(user.getChatId());
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+}
