24661cd8f8811e2d2a045c7429fbb60c1d254155
Jason <jpanovka@uoguelph.ca>
2022-03-10 00:26:38 -0500

    DAO and CardViews
    Created basic DAO functions and basic Cardviews for various app views

--- app/src/main/java/com/teamhowl/howl/controllers/PooledBlockDao.java
+++ app/src/main/java/com/teamhowl/howl/controllers/PooledBlockDao.java
@@ -1,7 +1,20 @@
 package com.teamhowl.howl.controllers;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Query;
+
+import com.teamhowl.howl.models.StashedBlock;
+
+import java.util.List;
 
 public interface PooledBlockDao extends Dao {
+    @Query("SELECT * FROM StashedBlock WHERE chat_id LIKE :chatId")
+    public abstract List<StashedBlock> findBlocksByChatId(String chatId);
 
+    @Query("SELECT * FROM StashedBlock")
+    public abstract List<StashedBlock> findAllBlocks();
+
+    @Delete
+    public void deleteBlocks(List<StashedBlock> chatId_blocks);
 }
--- app/src/main/java/com/teamhowl/howl/models/StashedBlock.java
+++ app/src/main/java/com/teamhowl/howl/models/StashedBlock.java
@@ -1,10 +1,12 @@
 package com.teamhowl.howl.models;
 
 import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
 
+@Entity
 public class StashedBlock {
 
     @PrimaryKey
