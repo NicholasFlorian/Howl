8c87ab8adc653ed37295da23ebe50ecf14bca073
Jason <jpanovka@uoguelph.ca>
2022-04-05 17:33:26 -0400

    Show Notification
    Made notification visible function

--- app/src/main/java/com/teamhowl/howl/utilities/Notifications.java
+++ app/src/main/java/com/teamhowl/howl/utilities/Notifications.java
@@ -2,11 +2,14 @@ package com.teamhowl.howl.utilities;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.teamhowl.howl.R;
 
@@ -26,9 +29,21 @@ public class Notifications extends AppCompatActivity {
         }
     }
 
+    Intent intent = new Intent(this, Notifications.class);
+    //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
     NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_notifications_black_24dp)
             .setContentTitle("New Message")
             .setContentText("...")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true);
+
+    public void showNotification() {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(1, builder.build());
+    }
 }
